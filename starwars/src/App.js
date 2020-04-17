7c4d24d70acae1dfdc27515e4f47629a479c2845
Emilio <ediaz-goico@me.bergen.edu>
2020-04-17 14:05:20 -0400

    finished MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
+import styled from "styled-components"
+
+const PageTitle = styled.h1`
+background-color: white;
+    width: 20%;
+`
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-flow: wrap;
+  width:100%;
+`
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +26,30 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [starWarsCharacters, setStarWarsCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://swapi.py4e.com/api/people/`)
+      .then((res) => {
+        setStarWarsCharacters(res.data.results);
+      })
+      .catch((error) => {
+        debugger;
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <PageTitle>Characters</PageTitle>
+      <CardContainer>
+        {
+        starWarsCharacters.map((character, index) => 
+          <Character key={index}  character={character} />
+      )
+      }
+      </CardContainer>
     </div>
   );
-}
-
+};
 export default App;
